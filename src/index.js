a8902ef81f6dbdf2c96bbce29c269ce044ea8ad6
Hernan Martinez Reumann <hmreumann@hotmail.com>
2022-11-02 17:15:30 -0300

    Playing with D3 Histogram

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as d3 from "d3";
+import { Treemap } from "./Treemap.js"
+import { Histogram } from "./Histogram.js"
+
+window.d3 = d3
+window.Treemap = Treemap
+window.Histogram = Histogram
