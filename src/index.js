da63bf22b40bf822c00fd3f539ee0cef0b771f2e
Hernan Martinez Reumann <hmreumann@hotmail.com>
2022-11-03 18:44:07 -0300

    Added barchart for 2022-election-fundrising

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import * as d3 from "d3";
 import { Treemap } from "./Treemap.js"
 import { Histogram } from "./Histogram.js"
+import { BarChart } from "./BarChart.js"
 
 window.d3 = d3
 window.Treemap = Treemap
 window.Histogram = Histogram
+window.BarChart = BarChart
