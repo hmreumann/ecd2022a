0f208647a49d7d129e40f877a7cbadc24c682ae9
Hernan Martinez Reumann <hmreumann@hotmail.com>
2022-11-03 19:04:49 -0300

    Change barchart to horizontal W44

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import * as d3 from "d3";
 import { Treemap } from "./Treemap.js"
 import { Histogram } from "./Histogram.js"
 import { BarChart } from "./BarChart.js"
+import { HorizontalBarChart } from "./HorizontalBarChart.js"
 
 window.d3 = d3
 window.Treemap = Treemap
 window.Histogram = Histogram
 window.BarChart = BarChart
+window.HorizontalBarChart = HorizontalBarChart
